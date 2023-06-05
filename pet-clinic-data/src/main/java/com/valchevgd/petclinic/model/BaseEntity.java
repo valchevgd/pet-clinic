9d2cf3e320ca0ba8f2fe91628f8e93d96d5afa68
Georgi D. Valchev <valchevgd@gmail.com>
2023-06-05 22:07:36 +0300

    Added BaseEntity. Closes #9.

--- pet-clinic-data/src/main/java/com/valchevgd/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/valchevgd/petclinic/model/BaseEntity.java
@@ -3,8 +3,11 @@ package com.valchevgd.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity {
-    private String name;
+public class BaseEntity implements Serializable {
+
+    private Long id;
 }
